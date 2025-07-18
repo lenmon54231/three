36db726becc34ba5f29ebe7b4535a646dac17bfa
limeng <limeng54231@163.com>
2025-07-18 11:57:32 +0800

    2025/7/18 11:57:26

--- src/views/ModelViewer/ModelViewer.tsx
+++ src/views/ModelViewer/ModelViewer.tsx
@@ -70,6 +70,7 @@ const ModelViewer: React.FC = () => {
            <ModelContent isTopView={isTopView} carColor={carColor} startAnim={startAnim} animDone={animDone} />
           <OrbitControls
             target={[0, 0, 0]}
+            maxPolarAngle={1.24}
             enabled={true}
             enableZoom={false}
             enableRotate={animDone}
